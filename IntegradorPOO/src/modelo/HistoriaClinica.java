2cfa859a0ee791d733a042cc22cc03eaa60e391b
AuxZarske <43278023+AuxZarske@users.noreply.github.com>
2019-06-13 09:49:11 -0300

    correccion de persistencia listo

--- IntegradorPOO/src/modelo/HistoriaClinica.java
+++ IntegradorPOO/src/modelo/HistoriaClinica.java
@@ -2,6 +2,7 @@
 package modelo;
 
 import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -25,7 +26,7 @@ public class HistoriaClinica {
     private Paciente paciente;
     
     @OneToMany(mappedBy = "hc")
-    private ArrayList<Registro> registros;
+    private List<Registro> registros;  //problema
     
     
     //Constructores
@@ -59,7 +60,7 @@ public class HistoriaClinica {
     }
 
     
-    public ArrayList<Registro> getRegistro() {
+    public List<Registro> getRegistro() {
         return registros;
     }
 
