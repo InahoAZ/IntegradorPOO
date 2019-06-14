580ba613446f620e2f67f7a70f7114f6e507b03d
AuxZarske <43278023+AuxZarske@users.noreply.github.com>
2019-06-14 09:56:01 -0300

    se modifico nombre de la ventana persona a pacientes

--- IntegradorPOO/src/Vista/VentanaModifPacientes.java
+++ IntegradorPOO/src/Vista/VentanaModifPacientes.java
@@ -9,12 +9,12 @@ package Vista;
  *
  * @author Arnold Zarske
  */
-public class VentanaModifPersona extends javax.swing.JFrame {
+public class VentanaModifPacientes extends javax.swing.JFrame {
 
     /**
      * Creates new form VentanaModifPersona
      */
-    public VentanaModifPersona() {
+    public VentanaModifPacientes() {
         initComponents();
     }
 
@@ -70,20 +70,21 @@ public class VentanaModifPersona extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VentanaModifPersona.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaModifPacientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VentanaModifPersona.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaModifPacientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VentanaModifPersona.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaModifPacientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VentanaModifPersona.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaModifPacientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new VentanaModifPersona().setVisible(true);
+                new VentanaModifPacientes().setVisible(true);
             }
         });
     }
--- IntegradorPOO/src/Vista/VentanaPacientes.java
+++ IntegradorPOO/src/Vista/VentanaPacientes.java
@@ -9,12 +9,12 @@ package Vista;
  *
  * @author Arnold Zarske
  */
-public class VentanaPersonas extends javax.swing.JFrame {
+public class VentanaPacientes extends javax.swing.JFrame {
 
     /**
      * Creates new form VentanaPersonas
      */
-    public VentanaPersonas() {
+    public VentanaPacientes() {
         initComponents();
     }
 
@@ -138,20 +138,21 @@ public class VentanaPersonas extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VentanaPersonas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaPacientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VentanaPersonas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaPacientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VentanaPersonas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaPacientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VentanaPersonas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaPacientes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new VentanaPersonas().setVisible(true);
+                new VentanaPacientes().setVisible(true);
             }
         });
     }
